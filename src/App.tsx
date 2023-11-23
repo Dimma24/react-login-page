668e9087233f01090935908b0e926d6d630f74f0
dimma24 <ozommadimma@gmail.com>
2023-11-23 16:22:58 +0100

    fixed the form bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Form from "./components/Form";
+// import Form from "./components/form";
 import FormHeader from "./components/FormHeader";
 import Button from "./components/Button";
 import "./styles/App.css";
